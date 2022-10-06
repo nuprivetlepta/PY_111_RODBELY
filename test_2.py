7188e5a0b33d082742a5b1bf288040d1345c2604
nuprivetlepta <stopkran510@gmail.com>
2022-10-06 20:12:38 +0300

    2nd test started

---
+++ test_2.py
@@ -0,0 +1,10 @@
+pass
+
+# def navi(start: (int, int), stop: (int, int)) -> int:
+#     our_field = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#     total_cost = 0
+#
+#     if start == stop:
+#         total_cost += our_field[start[0]][start[1]]
+#         return total_cost
+
