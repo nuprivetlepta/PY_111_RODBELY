e943b253135f51a025e8b68d5ce71459282635ba
nuprivetlepta <stopkran510@gmail.com>
2022-10-06 20:59:47 +0300

    4th test ended all works (exept mergesort)

--- test_4.py
+++ test_4.py
@@ -1,11 +1,16 @@
+import random as rnd
+
+
 def quicksort(lst):
     if len(lst) < 2:
         return lst
     else:
-        pivot = lst[0]
-        malishi = [i for i in lst[1:] if i <= pivot]
-        bolshi = [i for i in lst[1:] if i > pivot]
-        return quicksort(malishi) + [pivot] + quicksort(bolshi)
+        pivot_index = len(lst) // 2
+        pivot_value = lst[pivot_index]
+        malishi = [i for i in lst if i < pivot_value]
+        pivotiki = [i for i in lst if i == pivot_value]
+        bolshi = [i for i in lst if i > pivot_value]
+        return [quicksort(malishi) + pivotiki + quicksort(bolshi)]
 
 
 def selection(lst):
@@ -17,15 +22,45 @@ def selection(lst):
     return lst
 
 
+def merg_fn(left_side: list[int], right_side: list[int]):
+
+    sorted_res = []
+    cur_l_ind = 0
+    cur_r_ind = 0
+
+    while True:
+        cur_l_val = left_side[cur_l_ind]
+        cur_r_val = right_side[cur_r_ind]
+
+        if cur_l_val < cur_r_val:
+            sorted_res.append(cur_l_val)
+            cur_l_ind += 1
+        else:
+            sorted_res.append(cur_r_val)
+            cur_r_ind += 1
+
+        if cur_l_ind == len(left_side):
+            sorted_res.extend(right_side[cur_r_ind:])
+            break
+
+        elif cur_r_ind == len(left_side):
+            sorted_res.extend(left_side[cur_l_ind:])
+            break
+
+
 def merge(lst):
     if len(lst) == 1:
         return lst
-    else:
-        ar_1 = lst[:len(lst)//2]
-        ar_2 = lst[len(lst)//2:]
+
+    mid_ind = len(lst) // 2
+    ar_1 = merge(lst[: mid_ind])
+    ar_2 = merge(lst[mid_ind:])
+    return merg_fn(ar_1, ar_2)
 
 
 if __name__ == '__main__':
-    l = [1, 4, 11, 6546, 32, 566, 11, 6546, 987, 11, 645, 0, 87]
-    print(selection(l))
+    lafa = [rnd.randint(13, 26) for i in range(10**6)]
+    print(quicksort(lafa))
+    # print(merge(lafa))
+    print(selection(lafa))
 
